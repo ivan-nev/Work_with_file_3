ee818b8883b5b249191263e97a4e36bfe632bf4f
ivan-nev <i_nevakshenov@mail.ru>
2022-06-22 22:11:38 +0500

    edit make_sort_file

--- Sort_File_1-4.py
+++ Sort_File_1-4.py
@@ -18,8 +18,9 @@ def dict_sort_file(path_dir):
     sorted_dict = {k: v for k, v in sorted_tuples}
     return sorted_dict
 
-def make_sort_file(sorted_dict,path_dir):
+def make_sort_file(path_dir):
     os.remove('sort_file.txt')
+    sorted_dict = dict_sort_file(path_dir)
     for key, value in sorted_dict.items():
         # print(key, value)
         with open('sort_file.txt', 'a', encoding='utf-8') as f:
@@ -30,5 +31,5 @@ def make_sort_file(sorted_dict,path_dir):
                     f.write(str(line))
 
 
-make_sort_file(dict_sort_file('sorted'), 'sorted')
+make_sort_file('sorted')
 print (count_lines('sorted/1.txt'))
